8c923557d50bee1e67169e84ad707d9fe3db4425
Clive Baker <clive@clivebaker.com>
2012-07-10 12:53:09 +0100

    Added Image generation

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,10 +1,29 @@
 class ImageController < ApplicationController
+  
+  $names = [
+    {:id=>1, :name=>"Claudio Ortolina", :avatar=>"claudio.jpg" },
+    {:id=>2, :name=>"Clive Baker", :avatar=>"clive.jpg" },
+    {:id=>3, :name=>"Rimak Krivickas", :avatar=>"rimas.jpg" },
+    {:id=>4, :name=>"Spencer Turner", :avatar=>"spencer.jpg" }  
+    ]
+    
+  
+  
   def index
+    
+    
+    
+    
   end
 
   def generate
 
-    @name = params[:name]
+
+
+
+    person = $names.select {|f| f[:id] == params["name"].to_i  }.first
+    @name = person[:name]
+
 
     @position = params[:position]
     @outof = params[:outof]
@@ -16,7 +35,17 @@ class ImageController < ApplicationController
     source = File.join(Rails.root, 'public/ifp-world.jpg')
     @url_path_complete = File.join('/images/', filename )
     full_path_complete = File.join(Rails.root, 'public/images/', filename)
+    avatar_path = File.join(Rails.root, 'public/avatars', person[:avatar])
+    box_path = File.join(Rails.root, 'public', 'box_overlay.png')
+    
     img = MiniMagick::Image.open(source)
+
+    percentage = ((@position.to_f/@outof.to_f)*100).to_i
+    percentage = percentage==100 ? 99 : percentage
+
+    bounds = {:upper=>220, :lower=>5}
+
+    box_location = (bounds[:upper]-bounds[:lower]) * (100-percentage) / 100 + bounds[:lower]
 
 
     img.combine_options do |c|
@@ -30,6 +59,15 @@ class ImageController < ApplicationController
        c.draw "text 130,115 'out of #{@outof}'"
        c.fill "black"   
        c.draw "text 115,135 'created at #{time}'"
+       
+       c.draw "image over 13,49 0,0 '#{avatar_path}'"
+       
+       c.draw "image over #{box_location},142 0,0 '#{box_path}'"
+       c.pointsize "9" 
+       c.draw "text #{box_location+5},155 'TOP #{percentage}%'"
+       
+       
+       
     end
            
      img.write(full_path_complete)
